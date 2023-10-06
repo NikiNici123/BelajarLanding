e7e9021a3ac8d621640415882b0856be53fcd59d
NikiNici <61245651+NikiNici123@users.noreply.github.com>
2023-10-06 23:00:51 +0700

    Learn Responsive Page
    perubahan di susunan index HTML
    Landing responsive di elemen header dan navbar

--- Landing.js
+++ Landing.js
@@ -77,25 +77,28 @@ let buttonsDo = () => {
 const Header = () => {
     for(let i = 0; i<menu.Header.length;i++) {
         let IDParent = document.querySelector('#Header')
+        let divParent = document.createElement('div')
+        divParent.id = `Header${i}`
+        divParent.setAttribute('class', 'group flex lg:place-self-center lg:flex-col gap-y-10')
         let elemen = document.createElement('i')
-        elemen.setAttribute('class', menu.Header[i].class+ " border-slate-700 border-2 px-2 py-1 lg:px-5 lg:py-3 rounded-md hover:bg-slate-500 hover:border-white hover:text-red-300 hover:rounded-2xl transition-all duration-300 ease-in-out")
+        elemen.setAttribute('class', ` ${menu.Header[i].class} group hidden border-slate-700 border-2 px-2 py-1 lg:px-5 lg:py-3  lg:block rounded-md hover:bg-slate-500 hover:border-white hover:text-red-300 hover:rounded-2xl hover:shadow-2xl hover:shadow-slate-200 transition-all duration-300 ease-in-out`)
         elemen.setAttribute('id', menu.Header[i].id)
-        IDParent.appendChild(elemen)
+        IDParent.appendChild(divParent)
+        divParent.appendChild(elemen)
+        listInHeader(i, divParent.id)
     }
 }
 
-const listInHeader = () => {
-    menu.Header.forEach((element, index) => {
-       let IDParent = document.querySelector("#"+element.id)
-       let elemen = document.createElement('') 
-    });
-
+const listInHeader = (index,ParentID) => {
+      let IDParent = document.querySelector(`#${ParentID}`)
+      let elementUL = document.createElement('ul')
+      elementUL.setAttribute('class', 'flex flex-row justify-center ')
+      IDParent.appendChild(elementUL)
+      let elementList = document.createElement('a')
+      elementList.textContent = menu.Header[index].text
+      elementList.setAttribute('class' , 'group hidden text-sm text-white absolute py-5 px-5 bg-black lg:group-hover:block lg:group-hover:bg-slate-500 lg:group-hover:border-white lg:group-hover:text-red-300 lg:group-hover:rounded-2xl lg:group-hover:transition-all' )
+      elementUL.appendChild(elementList)
 }
-
-// const HeaderChild = () => {
-//     let IDParent = document.querySelector('#Project')
-//     for
-// }
 
 let scrollInto = (ButtonID, TargetClass) => {
     let ButtonProject = document.querySelector(`#${ButtonID}`)
