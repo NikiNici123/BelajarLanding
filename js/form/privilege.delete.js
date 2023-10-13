33cf8edfe66b677bd165eb3cf7a4916944458e29
NikiNici <61245651+NikiNici123@users.noreply.github.com>
2023-10-13 22:32:56 +0700

    bulk delete

--- js/form/privilege.delete.js
+++ js/form/privilege.delete.js
@@ -5,10 +5,17 @@ form_delete.addEventListener("submit", (e) => {
   let CheckboxValue = document.querySelectorAll("input[type=checkbox]")
   let data_id = document.querySelectorAll("#data_Id")
   let data_url = document.querySelectorAll("#data_Url")
-  
+
   CheckboxValue.forEach((element,index) => {
     if (element.checked) {
-      Data_Delete(parseInt(data_id[index].textContent), data_url[index].textContent)
+      if (element.checked > 1) {
+        for(let i = 0; i < element.checked.length ; i++) {
+          Data_Delete(parseInt(data_id[index].textContent), data_url[index].textContent)
+        }
+      }
+      else {
+        Data_Delete(parseInt(data_id[index].textContent), data_url[index].textContent)
+      }
     } 
   })
   setTimeout(() => window.location.reload(), 500)
