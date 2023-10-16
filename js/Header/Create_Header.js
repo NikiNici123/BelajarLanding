6420fabd212df4538e12009a3c7131f4494f7240
NikiNici <61245651+NikiNici123@users.noreply.github.com>
2023-10-16 16:06:01 +0700

    Responsive Rework main Page HTML

--- js/Header/Create_Header.js
+++ js/Header/Create_Header.js
@@ -1,16 +1,17 @@
 
-coba_Menu().then(menu => {
+fetch_Menu().then(menu => {
     for(let i = 0; i<menu.Header.length;i++) {
         let IDParent = document.querySelector('#Header')
         let divParent = document.createElement('div')
         divParent.id = `Header${i}`
         divParent.setAttribute('class', 'group flex lg:place-self-center lg:flex-col gap-y-10')
-        let elemen = document.createElement('i')
-        elemen.setAttribute('class', ` ${menu.Header[i].class} group  right-0 border-slate-700 border-2 px-2 py-1 lg:px-5 lg:py-3 lg:block rounded-md hover:bg-slate-500 hover:border-white hover:text-red-300 hover:rounded-2xl hover:shadow-2xl hover:shadow-slate-200 transition-all duration-300 ease-in-out`)
+        let elemen = document.createElement('a')
+        elemen.textContent = menu.Header[i].text
+        elemen.setAttribute('class', `group px-2 py-1 text-gray-400 lg:px-5 lg:py-3 lg:block rounded-md hover:border-white hover:text-slate-950 hover:shadow-2xl hover:shadow-slate-200 transition-all duration-300 ease-in-out`)
         elemen.setAttribute('id', menu.Header[i].id)
         IDParent.appendChild(divParent)
         divParent.appendChild(elemen)
-        listInHeader(i, divParent.id, menu)
+        // listInHeader(i, divParent.id, menu)
     }
 })
 
--- js/Header/fetchHeader.js
+++ js/Header/fetchHeader.js
@@ -1,4 +1,4 @@
-const coba_Menu = async () => {
+const fetch_Menu = async () => {
     const panggil = await fetch("Data/menu.json")
     const response = await panggil.json()
     return response
